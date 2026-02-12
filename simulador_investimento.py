9227226171ad0452bfd428f9e22793cdf60ba56b
aurelioparreira <profaurelioparreira@gmail.com>
2026-02-12 09:29:50 -0300

    Update simulador_investimento.py

--- simulador_investimento.py
+++ simulador_investimento.py
@@ -1,9 +1,8 @@
-python
-
-valor = 10000
-taxa = 0.12
-anos = 5
+valor = float(input("Valor inicial: "))
+taxa = float(input("Taxa anual (ex: 0.12 para 12%): "))
+anos = int(input("Número de anos: "))
 
 montante = valor * (1 + taxa) ** anos
 
-print("Montante final:", round(montante, 2))
+print("\nResultado da simulação:")
+print("Valor final:", round(montante, 2))
