5bba5fbb4bdb7d116182da5499f7635bfcd60ecd
aurelioparreira <profaurelioparreira@gmail.com>
2026-02-13 11:32:51 -0300

    Delete simulador_investimento.py

--- simulador_investimento.py
+++
@@ -1,8 +0,0 @@
-valor = float(input("Valor inicial: "))
-taxa = float(input("Taxa anual (ex: 0.12 para 12%): "))
-anos = int(input("Número de anos: "))
-
-montante = valor * (1 + taxa) ** anos
-
-print("\nResultado da simulação:")
-print("Valor final:", round(montante, 2))
